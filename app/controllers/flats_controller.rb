ce933aecfb7aee24c7b0ff220a28d943ccd6a749
Firas Jamaleddin <firasabdelhadi94@aol.com>
2022-09-03 09:00:14 -0700

    created views

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,22 +1,41 @@
 class FlatsController < ApplicationController
 
+  # def index
+  #   @flats = Flat.all
+  # end
+
   def index
     @flats = Flat.all
+    if params[:query].present?
+      @flats = Flat.where(category: params[:query])
+    end
   end
 
   def new
     @flat =  Flat.new
   end
 
-  # def show
-  # end
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  # def create
-  # end
+  def show
+    @flat = Flat.find(params[:id])
+  end
 
-  # def update
-  # end
+  private
 
-  # def destroy
-  # end
+  def article_params
+    params.require(:article).permit(:title, :body, photos: [])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:title, :address, :price, :description, :pet_min_size, :pet_max_size, :photos)
+  end
 end
