d49bea6300f71fbef36e8445c19a3d5b44a61d95
Firas Jamaleddin <firasabdelhadi94@aol.com>
2022-09-27 12:41:31 -0700

    profile ad pet pic updated

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
 
   enum pet_max_size: {
     small: 0,
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :pets, dependent: :destroy
   has_many :flats, dependent: :destroy
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
 end
